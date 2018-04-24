f8abb8394fd3cf820de346c7c14cdb80b3817e6e
Дмитрий <32355073+xex238@users.noreply.github.com>
2018-04-24 13:12:20 +0300

    Create main.cpp

---
+++ Discrete_math/course_work_new_version/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "cows_and_bulls.h"
+using namespace std;
+
+int main()
+{
+    int count_of_numbers{ 0 };
+    cout << "Enter count of numbers" << endl;
+    cin >> count_of_numbers;
+
+    while ((count_of_numbers <= 3) || (count_of_numbers > 10))
+    {
+        cout << "Your data is wrong. Try again" << endl;
+        cin >> count_of_numbers;
+    }
+
+    Cows_and_bulls cb = Cows_and_bulls(count_of_numbers);
+
+    while (!cb.Step_1())
+    {
+        //cb.Step_1();
+    }
+}
