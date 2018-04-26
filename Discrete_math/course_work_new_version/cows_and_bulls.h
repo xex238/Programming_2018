b7b334790cec6985415ef728ebae540beb70b8b8
Дмитрий <32355073+xex238@users.noreply.github.com>
2018-04-27 00:04:54 +0300

    Update cows_and_bulls.h

--- Discrete_math/course_work_new_version/cows_and_bulls.h
+++ Discrete_math/course_work_new_version/cows_and_bulls.h
@@ -6,7 +6,11 @@ class Cows_and_bulls
 {
 public:
     Cows_and_bulls(int count_of_numbers_);
+    // Метод для заполнения массива output[i]
+    void Filling_output(int x_left_part, int x_right_part);
+    void Moving_array(int* arr, int* size);
     bool Step_1();
+    bool Step_2();
 private:
     int count_of_numbers{ 0 };
 
@@ -36,6 +40,8 @@ private:
     int bulls{ 0 };
 
     int sum_cows{ 0 };
+
+    int id_check{ 0 };
 };
 
 #endif // !Cows_and_bulls_
